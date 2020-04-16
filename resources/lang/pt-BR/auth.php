fd28ad6b4094f3bcb69d916d69c3188fac8d3ee5
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-16 17:57:33 -0400

    Remove Welcome route

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+    'unauthorizated' => 'Não autorizado.',
+    'created' => 'Usuário criado com sucesso!',
+    'no-activated' => 'O usuário não foi ativado.',
+    'firebase-save-failed' => 'Salvar usuário no serviço do Firebase falhou.',
+    'firebase-login-failed' => 'O logout do usuário no serviço do Firebase falhou.',
+    'password-mismatch' => 'Senha incorreta.',
+    'user-not-found' => 'Usuário não existe.',
+    'login-success' => 'Você foi desconectado com sucesso.',
+
+];
