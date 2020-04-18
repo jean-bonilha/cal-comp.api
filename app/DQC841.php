eab10486221d5454d94948f8408d014c457d03cc
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-17 21:33:05 -0400

    Makes setup to DQC841 Model

--- app/DQC841.php
+++ app/DQC841.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class DQC841 extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'DQC841';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'FAT_PART_NO',
+        'PARTS_NO',
+        'UNT_USG',
+        'DESCRIPTION',
+        'REF_DESIGNATOR'
+    ];
+
+    /**
+     * The name of the "created_at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'CREATE_DT';
+
+    /**
+     * The name of the "updated_at" column.
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'UPDATE_DT';
 }
