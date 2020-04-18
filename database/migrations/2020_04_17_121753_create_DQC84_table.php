86557f51b37f282b555aae99bf99af30b56f166f
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-18 12:12:50 -0400

    Fix use statement

--- database/migrations/2020_04_17_121753_create_DQC84_table.php
+++ database/migrations/2020_04_17_121753_create_DQC84_table.php
@@ -1,4 +1,8 @@
-<?php use Illuminate\Database\Migrations\Migration; use Illuminate\Database\Schema\Blueprint; use Illuminate\Support\Facades\Schema;
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateDQC84Table extends Migration
 {
