2f426a3abd5268d02f467df4d9fdc7cde2893238
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-18 12:24:04 -0400

    Add dqc84 routes and yours resources

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('dqcmodel', 'DQCMODELController');
+
+Route::apiResource('dqc84', 'DQC84Controller');
