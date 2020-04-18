2fd6c00e63532b63a9496db6b4d70e61b4076087
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2020-04-18 15:26:38 -0400

    Setup DQC841Controller methods

--- app/Http/Controllers/DQC841Controller.php
+++ app/Http/Controllers/DQC841Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use Validator;
+use App\DQC84;
 use App\DQC841;
 use Illuminate\Http\Request;
 
@@ -14,17 +17,7 @@ class DQC841Controller extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return DQC841::all();
     }
 
     /**
@@ -35,7 +28,25 @@ class DQC841Controller extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'FAT_PART_NO' => 'required|min:1|max:20',
+            'PARTS_NO' => 'required|min:10|max:20',
+            'UNT_USG' => 'required|min:1|max:11',
+            'DESCRIPTION' => 'min:2',
+            'REF_DESIGNATOR' => 'min:2',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!DQC84::find($request->FAT_PART_NO)) {
+            return response()->json(['message' => 'DQC84 nao foi encontrado.'], 404);
+        }
+
+        $dqc841 = DQC841::create($request->all());
+
+        return response()->json($dqc841, 201);
     }
 
     /**
@@ -44,20 +55,15 @@ class DQC841Controller extends Controller
      * @param  \App\DQC841  $dQC841
      * @return \Illuminate\Http\Response
      */
-    public function show(DQC841 $dQC841)
+    public function show($dQC841)
     {
-        //
-    }
+        $dqc841 = DQC841::find($dQC841);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\DQC841  $dQC841
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(DQC841 $dQC841)
-    {
-        //
+        if (!$dqc841) {
+            return response()->json(['message' => 'DQC841 nao foi encontrado.'], 404);
+        }
+
+        return response()->json($dqc841); 
     }
 
     /**
@@ -67,9 +73,53 @@ class DQC841Controller extends Controller
      * @param  \App\DQC841  $dQC841
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, DQC841 $dQC841)
+    public function update(Request $request, $dQC841)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'FAT_PART_NO' => 'required|min:1|max:20',
+            'PARTS_NO' => 'required|min:10|max:20',
+            'UNT_USG' => 'required|min:1|max:11',
+            'DESCRIPTION' => 'required|min:2',
+            'REF_DESIGNATOR' => 'min:2',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!DQC84::find($request->FAT_PART_NO)) {
+            return response()->json(['message' => 'DQC84 nao foi encontrado.'], 404);
+        }
+
+        if (DQC841::find($dQC841)) {
+
+            $dataUpdate = [
+                $request->FAT_PART_NO,
+                $request->PARTS_NO,
+                $request->UNT_USG,
+                $request->DESCRIPTION,
+            ];
+
+            $sql = 'update DQC841 set FAT_PART_NO = ?, PARTS_NO = ?, UNT_USG = ?, DESCRIPTION = ?';
+
+            if ($request->REF_DESIGNATOR) {
+                array_push($dataUpdate, $request->REF_DESIGNATOR);
+                $sql .= ', REF_DESIGNATOR = ?';
+            }
+
+            array_push($dataUpdate, $dQC841);
+
+            $sql .= ' where ID = ?';
+
+            DB::update(
+                $sql,
+                $dataUpdate,
+            );
+
+            return response()->json(['message' => 'O DQC841 foi atualizado com sucesso.'], 200);
+        }
+
+        return response()->json(['message' => 'DQC841 nao foi encontrado.'], 404);
     }
 
     /**
@@ -78,8 +128,14 @@ class DQC841Controller extends Controller
      * @param  \App\DQC841  $dQC841
      * @return \Illuminate\Http\Response
      */
-    public function destroy(DQC841 $dQC841)
+    public function destroy($dQC841)
     {
-        //
+        $deleted = DB::delete('delete from DQC841 where id = ?', [$dQC841]);
+
+        if ($deleted) {
+            return response()->json(['message' => 'O DQC841 foi excluido com sucesso.'], 200);
+        }
+
+        return response()->json(['message' => 'DQC841 nao foi encontrado.'], 404);
     }
 }
